1a4027736c6cffad9ba3ed2f229c2ef3c1ca7f84
Alex Gobert <alexandergobert@utexas.edu>
2022-11-02 13:51:55 -0500

    added initial template code for song writing

--- servo_controller.ino
+++ servo_controller.ino
@@ -1,41 +1,54 @@
 #include <Servo.h>
+#include <map>
 
 // create servo object to control servos
 const int numServos = 1;
 Servo servos[numServos];
 int positions[numServos];
-// int pins[numServos] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-int pins[numServos] = {44};
+int pins[numServos] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+// note to Servo
+
+std::map<string, Servo> servo_dict = {
+    {}
+};
 
 void setup() {
     Serial.begin(31250);
     
+    // attach Servos to pins
     for (int i = 0; i < numServos; i++) {
         servos[i].attach(pins[i]);
     }
+
+    // create servo dict
+    for (int i = 0; i < numServos; i++) {
+
+    }
+
+    // read file
 }
 
 void loop() {
-
-    // for (int i = 0; i < numServos; i++) {
-    //     writeNote(servos[i]);
-    // }
-
-    writeAll(0);
-    delay(200);
-
-    writeAll(35);
-    delay(1000);
+    int dummy = 0;
 }
 
-void writeAll(int pos) {
-    for (int i = 0; i < numServos; i++) {
-        servos[i].write(pos);
+void playSong(int bpm, int numBeats, std::map<int, std::vector> beats) {
+    double beatDuration = 60 * 1.0 / bpm; // seconds
+
+    for (int i = 0; i < numBeats; i++) {
+        notes = beats[i];
+
+        for (int n = 0; n < notes.length; n++) {
+            writeNote(servo_dict[notes[n]]);
+        }
+
+        delay(beatDuration * 1000) // delay in milliseconds
     }
 }
 
-// void writeNote(Servo servo) {
-//     servo.write(0);
-//     delay(200);
-//     servo.write(35);
-// }
+void writeNote(Servo servo) {
+    servo.write(0);
+    delay(200);
+    servo.write(35);
+}
