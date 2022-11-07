1133580535613b018d8d34d32145cde3f0ece01b
Alex Gobert <alexandergobert@utexas.edu>
2022-11-07 13:28:20 -0600

    added user input

--- servo_controller.ino
+++ servo_controller.ino
@@ -1,4 +1,3 @@
-
 #include <ArduinoSTL.h>
 #include <system_configuration.h>
 #include <unwind-cxx.h>
@@ -11,6 +10,10 @@ int baudRate = 9600;
 // pin constants
 int servoPins[numServos] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50};
 int ledPins[numServos] = {23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51};
+std::map<uint8_t, uint8_t> ledStates = {
+    {HIGH, LOW},
+    {LOW, HIGH}
+};
 
 // create servo object to control servos
 Servo servos[numServos];
@@ -20,7 +23,10 @@ int servoDelay = 200;
 String notes[numServos] = {"G5","A6", "B6", "C6", "D6", "E6", "F6", "G6", "A7", "B7", "C7", "D7", "E7", "F7", "G7"};
 
 // note to Servo
-std::map<String, Servo> servo_dict;
+std::map<String, Servo> servoDict;
+
+// note to LED pin
+std::map<String, int> ledDict;
 
 // songs
 std::map<int, std::vector<String>> happyBdayBeats = {
@@ -51,7 +57,7 @@ std::map<int, std::vector<String>> happyBdayBeats = {
     {49, {"C7"}},
 };
 int happyBdayBPM = 180;
-int happyBdayNumBeats = 3;
+int happyBdayNumBeats = 54;
 
 std::map<int, std::vector<String>> tempBeats = {
     {1, {"G6"}},
@@ -65,13 +71,16 @@ void setup() {
     
     for (int i = 0; i < numServos; i++) {
         Servo curServo = servos[i];
+        int curLED = ledPins[i];
+        String curNote = notes[i];
         
         curServo.attach(servoPins[i]);
 
-        servo_dict[notes[i]] = curServo;
+        servoDict[curNote] = curServo;
+        ledDict[curNote] = curLED;
 
         // define LED pins
-        pinMode(i, OUTPUT);
+        pinMode(curLED, OUTPUT);
     }
 
 }
@@ -79,9 +88,40 @@ void setup() {
 // Arduino loop function
 // Can be used for user input (pick and play song)
 void loop() {
-    Serial.println("NEW ITERATION");
+    showOptions();
+
+    // empty loop to halt for user input
+    while (Serial.available() == 0) {} 
+    
+    int input = Serial.parseInt();
+    
+    int bpm;
+    int numBeats;
+    std::map<int, std::vector<String>> beats;
+
+    // set song data based on user input
+    switch (input) {
+        case 1: // Happy Birthday
+            bpm = happyBdayBPM;
+            numBeats = happyBdayNumBeats;
+            beats = happyBdayBeats;
+
+        default: // Invalid selection
+            Serial.println("Please make a valid selection");
+    }
+
+    playSong(bpm, numBeats, beats);
+
+    delay(1000);
+}
+
+void showOptions() {
+    Serial.println("Please pick a song from the following list of songs: ");
     Serial.println();
-    playSong(happyBdayBPM, happyBdayNumBeats, tempBeats);
+    Serial.println("\t1) Happy Birthday");
+    Serial.println("\t2) Song 2");
+    Serial.println("\t3) Song 3");
+    Serial.println();
 }
 
 // Plays a song given data about it
@@ -111,18 +151,23 @@ void playSong(int bpm, int numBeats, std::map<int, std::vector<String>> beats) {
 }
 
 // plays a note
-// @param 
+// @param notes is a vector of strings that represent the notes to play
 void writeNote(std::vector<String> notes) {
     moveServosAtNote(notes, 0);
     delay(servoDelay);
     moveServosAtNote(notes, 35);
 }
 
+// actuates servos at a note to a given angle
+// @param notes is a vector of notes that need to be played
+// @param angle is an int that represents a target angle for servos
 void moveServosAtNote(std::vector<String> notes, int angle) {
-    // int ledPin; // TODO
-    // digitalWrite(ledPin, HIGH);
-    
     for (String note: notes) {
-        servo_dict[note].write(angle);
+        Servo servo = servoDict[note];
+        int ledPin = ledDict[note];
+        uint8_t curState = digitalRead(ledPin);
+
+        servo.write(angle);
+        digitalWrite(ledPin, ledStates[curState]);
     }
 }
